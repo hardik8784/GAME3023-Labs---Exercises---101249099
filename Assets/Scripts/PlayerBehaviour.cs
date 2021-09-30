dcf404241ad559c58d1bbd86b4670912260b1920
Hardik <hardik8784@gmail.com>
2021-09-30 13:10:28 -0400

    Created 2 Levels and implemented the collision
    Added the 2 Levels into the scene with the collision between player and world

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBehaviour : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class PlayerBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+       
         Debug.Log("Game Started..!!");
         Debug.Log("Kem Cho World..??");
     }
@@ -21,6 +23,14 @@ public class PlayerBehaviour : MonoBehaviour
         float InputY = Input.GetAxisRaw("Vertical");
 
         transform.position += new Vector3(InputX, InputY, 0) * moveSpeed * Time.deltaTime;
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.tag == "Door")
+        {
+            Debug.Log("Collision with Door");
+            SceneManager.LoadScene("Level2");
+        }
     }
 }
